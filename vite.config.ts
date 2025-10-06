5275e17ae228b1d1e75979ce37fc6f198812a6a2
Theo Grandy <bpgrandy@gmail.com>
2025-10-06 17:13:09 -0400

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/wq2/', // GitHub Pages subdirectory
+  base: '/WombatQuest/', // GitHub Pages subdirectory
   resolve: {
     alias: {
       '@': './',
